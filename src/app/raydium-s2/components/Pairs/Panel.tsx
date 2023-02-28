a834e2e8a880876231a9aec2c323982ac937b276
zhanggongying <edsolater@qq.com>
2023-02-28 17:42:25 +0800

    🚧

--- src/app/raydium-s2/components/Pairs/Panel.tsx
+++ src/app/raydium-s2/components/Pairs/Panel.tsx
@@ -5,8 +5,6 @@ import { ApiJsonPairInfo } from 'test-raydium-sdk-v2'
 export function PairsPanel(rawProps: { infos: ApiJsonPairInfo[] }) {
   const props = useKitProps(rawProps)
   const pivProps = props
-  createEffect(() => console.log('props.infos: ', props, props.infos.length))
-  console.log('props.infos: ', props.infos)
   return (
     <Piv>
       <For each={props.infos}>
