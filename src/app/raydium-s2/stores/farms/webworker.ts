ae953398a50f57b4629196c0540814bb14f01055
zhanggongying <edsolater@qq.com>
2023-03-07 16:28:39 +0800

    🚧 add farm sdk parse

--- src/app/raydium-s2/stores/farms/webworker.ts
+++ src/app/raydium-s2/stores/farms/webworker.ts
@@ -2,33 +2,38 @@ import { registMessageReceiver } from '../common/webworker/worker_sdk'
 import { FarmPoolJsonInfo, FetchFarmsJsonPayloads, FetchFarmsSDKInfoPayloads, SdkParsedFarmInfo } from './type'
 import { fetchFarmJsonInfo } from './fetchFarmJson'
 import { getConnection } from '../common/utils/getConnection'
-import { Farm, FarmFetchMultipleInfoParams, jsonInfo2PoolKeys } from '@raydium-io/raydium-sdk'
+import { Farm, FarmFetchMultipleInfoParams } from '@raydium-io/raydium-sdk'
 import { toPub } from '../common/utils/pub'
+import { createSubscribable } from '../../../../packages/fnkit/createSubscribable'
+import { jsonInfo2PoolKeys } from '../../modules/sdkTools/jsonInfo2PoolKeys'
 
-let cachedFarmJsons: FarmPoolJsonInfo[] = []
+const [cachedFarmJsons$, setCachedFarmJsons] = createSubscribable<FarmPoolJsonInfo[]>([])
 
 export function registInWorker() {
   registMessageReceiver<FetchFarmsJsonPayloads>('fetch raydium farms info', (payload) =>
     fetchFarmJsonInfo(payload).then((infos) => {
-      cachedFarmJsons = infos ?? []
+      setCachedFarmJsons(infos ?? [])
       return infos
     })
   )
 
   registMessageReceiver<FetchFarmsSDKInfoPayloads>('parse raydium farms info sdk list', async ({ rpcUrl, owner }) => {
     const connection = getConnection(rpcUrl)
-    console.log('cachedFarmJsons: ', cachedFarmJsons)
-    // TEMPLY don't use
-    const sdkParsedInfos = await getSdkParsedFarmInfo(
-      {
-        connection,
-        pools: cachedFarmJsons.map(jsonInfo2PoolKeys),
-        owner: toPub(owner),
-        config: { commitment: 'confirmed' }
-      },
-      { jsonInfos: cachedFarmJsons }
-    )
-    console.log('sdkParsedInfos: ', sdkParsedInfos)
+    const { abort } = cachedFarmJsons$.subscribe(async (cachedFarmJsons) => {
+      console.log('cachedFarmJsons: ', cachedFarmJsons)
+      if (!cachedFarmJsons?.length) return
+      const sdkParsedInfos = await getSdkParsedFarmInfo(
+        {
+          connection,
+          pools: cachedFarmJsons.map(jsonInfo2PoolKeys),
+          owner: toPub(owner),
+          config: { commitment: 'confirmed' }
+        },
+        { jsonInfos: cachedFarmJsons }
+      )
+      // if (sdkParsedInfos.length) return abort
+      console.log('sdkParsedInfos: ', sdkParsedInfos)
+    })
   })
 }
 
@@ -39,8 +44,9 @@ export async function getSdkParsedFarmInfo(
     jsonInfos: FarmPoolJsonInfo[]
   }
 ): Promise<SdkParsedFarmInfo[]> {
+  console.log('options: ', options)
   const rawInfos = await Farm.fetchMultipleInfoAndUpdate(options)
-  console.log('rawInfos: ', rawInfos )
+  console.log('rawInfos: ', rawInfos)
   const result = options.pools.map((pool, idx) => {
     return {
       ...payload.jsonInfos[idx],
