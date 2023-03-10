24da077c50981c8835e0234601ef1d5265b129dd
zhanggongying <edsolater@qq.com>
2023-03-10 14:18:03 +0800

    💄 strip style type for easier read

--- src/app/raydium-s2/pages/Farms.tsx
+++ src/app/raydium-s2/pages/Farms.tsx
@@ -1,6 +1,8 @@
 import { Piv } from '@edsolater/piv'
 import { createRef, useElementSize } from '@edsolater/pivkit'
 import { createMemo, For, Show } from 'solid-js'
+import { cssColors } from '../../../packages/pivkit/styles/cssColors'
+import { tailwindPaletteColors } from '../../../packages/pivkit/styles/tailwindPaletteColors'
 import { NavBar } from '../components/NavBar'
 import { useFarmStore } from '../stores/farms/store'
 
@@ -23,7 +25,8 @@ export function FarmPanel() {
           borderRadius: 4,
           display: 'flex',
           flexDirection: 'column',
-          gap: 4
+          gap: 2,
+          '> :nth-child(2n)': { background: tailwindPaletteColors.gray50 }
         }}
       >
         <For each={farmStore.allFarmJsonInfos}>
