07fb30b8e4368690870bd8f4cdd40049d1ffc486
edsoalter <edsolater@qq.com>
2023-04-05 01:39:22 +0800

    🚧 predefineFunctionParams

--- src/packages/pivkit/utils/accessifyProps.ts
+++ src/packages/pivkit/utils/accessifyProps.ts
@@ -1,4 +1,4 @@
-import { AnyObj, shrinkFn, unifyItem } from '@edsolater/fnkit'
+import { AnyFn, AnyObj, isFunction, isObject, mergeObjectsWithConfigs, shrinkFn, unifyItem } from '@edsolater/fnkit'
 import { ValidController } from '../../piv/types/tools'
 
 /**
@@ -24,7 +24,12 @@ export function useAccessifiedProps<P extends Record<string, any>, Controller ex
         enumerable: true,
         get() {
           if (key.startsWith('on') || key.endsWith('Fn')) {
-            return props[key]
+            const v = props[key]
+            if (controller && isFunction(v)) {
+              return predefineFunctionParams(v, [controller])
+            } else {
+              return v
+            }
           } else {
             return shrinkFn(props[key], [controller])
           }
@@ -34,4 +39,24 @@ export function useAccessifiedProps<P extends Record<string, any>, Controller ex
     }, {} as PropertyDescriptorMap)
   ) as DeAccessify<P>
   return accessifiedProps
+}
+
+function predefineFunctionParams<F extends AnyFn, P extends any[] = Parameters<F>>(originalFn: F, preParams: P): F {
+  // @ts-expect-error no need to check
+  return {
+    [originalFn.name]: (...args: unknown[]) => originalFn(...shallowMergeTwoArray(preParams, args))
+  }[originalFn.name]
+}
+
+// TODO: move to fnkit
+function shallowMergeTwoArray(old: any[], arr2: any[]) {
+  return Array.from({ length: Math.max(old.length, arr2.length) }, (_, i) => {
+    const va = arr2[i]
+    const vb = old[i]
+    if (isObject(va) && isObject(vb)) {
+      return { ...va, ...vb }
+    } else {
+      return va ?? vb
+    }
+  })
 }
