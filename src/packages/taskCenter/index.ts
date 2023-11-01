567d9c6ccc2d5dfb38bae48989edeaed642ee25e
edsolater <edsolater@qq.com>
2023-11-01 09:23:26 +0800

    💩

--- src/packages/taskCenter/index.ts
+++ src/packages/taskCenter/index.ts
@@ -1,23 +1,27 @@
-import { map } from '@edsolater/fnkit'
+import { MayPromise, map } from '@edsolater/fnkit'
+import { P } from 'vitest/dist/reporters-5f784f42'
 
 type TaskKey = string
 type TaskPayload = any
+type TaskResult = any
 type TaskResultFromQueue = any
-type TaskAction<Result> = (result: Result) => void
+type TaskAction<Result = any> = (result: Result) => void
 type TaskController = {
   /** remove from queue */
   cancel: () => void
 }
-type TaskQueueHandler = (payloads: Set<TaskPayload>) => Map<TaskPayload, TaskController>
+type TaskHandler<TaskPayload = any, TaskResult = any> = (
+  payloads: Set<TaskPayload>,
+) => MayPromise<Map<TaskPayload, TaskResult>>
 
 function taskCenter<Config1 extends Record<TaskKey, { payload: TaskPayload; result: any }>>(
-  config: Record<keyof Config1, TaskQueueHandler>,
+  config: Record<keyof Config1, TaskHandler>,
 ) {
   type Config = {
     [K in keyof Config1]: {
       payload: Config1[K]['payload']
       result: Config1[K]['result']
-      queueHandler: TaskQueueHandler
+      queueHandler: TaskHandler
     }
   }
 
@@ -59,4 +63,24 @@ function taskCenter<Config1 extends Record<TaskKey, { payload: TaskPayload; resu
   return {
     addTask,
   }
+}
+
+const _taskCenter = new WeakMap<TaskHandler, Set<TaskPayload>>()
+const _taskHandlersTimoutids = new WeakMap<TaskHandler, ReturnType<typeof setTimeout>>()
+export function runInNextLoop<Payload, Result>(
+  taskHandler: TaskHandler<Payload, Result>,
+  taskPayload: Payload,
+): Promise<Result> {
+  _taskCenter.set(taskHandler, new Set(_taskCenter.get(taskHandler)).add(taskPayload)
+
+  clearTimeout(_taskHandlersTimoutids.get(taskHandler))
+  const newTimeoutid = setTimeout(() => {
+    const payloads = _taskCenter.get(taskHandler)
+    if (payloads?.size) {
+      taskHandler(payloads)
+    }
+    _taskCenter.delete(taskHandler)
+    _taskHandlersTimoutids.delete(taskHandler)
+  }, 0)
+  _taskHandlersTimoutids.set(taskHandler, newTimeoutid)
 }
