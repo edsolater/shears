2e545a206caf6bd2e8ecf70522252c85e4460213
zhanggongying <edsolater@qq.com>
2023-11-10 18:46:50 +0800

    💡 smart should have update action

--- src/packages/conveyor/basic.ts
+++ src/packages/conveyor/basic.ts
@@ -1,15 +1,27 @@
+import { Subscribable } from '@edsolater/fnkit'
+
+type Atom<T> = {
+  value: Subscribable<T>
+  /** so, u can 'mute' the action  */
+  isSubscribedBy?: T[]
+  updateAction?: (oldValue: T) => void
+}
+
+type SmartStore = {
+  xxx: Atom<unknown>
+}
+
 type SmartStoreOption = {}
 
 type SmartStoreDefaultValue = Record<keyof any, any>
 
 /** create Store by subscribable */
 export function smartStore<T extends SmartStoreDefaultValue>(defaultValue?: T, options?: SmartStoreOption) {
-  const innerStore: {[K in keyof T]: T[]} = {}
+  const innerStore: { [K in keyof T]: T[] } = {}
   if (defaultValue) {
     const defaultSymbolPairs = Object.getOwnPropertySymbols(defaultValue).map((key) => [key, defaultValue[key]])
     const defaultStringPairs = Object.entries(defaultValue)
     for (const [key, value] of defaultSymbolPairs.concat(defaultStringPairs)) {
-      
     }
   }
 }
