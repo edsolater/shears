08b6506330acab02e9825b1ffd2d64fea32dd07c
edsolater <edsolater@qq.com>
2023-11-25 15:46:31 +0800

    ✨ assignObjectWithConfigs

--- src/packages/fnkit/assignObject.ts
+++ src/packages/fnkit/assignObject.ts
@@ -6,33 +6,68 @@ import { isObjectLike } from '@edsolater/fnkit'
  * const patcher = {a: 2, c: 'hello'}
  * const c = assignObject(obj, patcher) //=> {b: 1, a: 2, c: 'hello'}
  */
-export function assignObject(obj: object, propertyPairs: object, onAssign?: (key: keyof any, value: any) => void) {
-  Reflect.ownKeys(propertyPairs).forEach((key) => {
-    Object.defineProperty(obj, key, Object.getOwnPropertyDescriptor(propertyPairs, key)!)
-  })
+export function assignObject<T extends object>(obj: T, propertyPairs: object): T {
+  assignObjectWithConfigs(obj, propertyPairs, simpleObectAssign)
+  return obj
 }
 
-//TODO: move to fnkit
-function forEachObjectEntries<T extends object>(obj: T, handler: (entry: [keyof T, T[keyof T]]) => void) {
-  Reflect.ownKeys(obj).forEach((key) => {
-    handler([key as keyof T, obj[key as keyof T]])
+type AttachFn = (payloads: {
+  key: keyof any
+  /** this maybe not obj */
+  obj: object
+  originalValue: any
+  patchValue: any
+}) => void
+
+const simpleObectAssign = ({
+  obj,
+  key,
+  patchValue,
+}: {
+  key: keyof any
+  /** this maybe not obj */
+  obj: object
+  originalValue: any
+  patchValue: any
+}): object =>
+  Object.defineProperty(obj, key, {
+    configurable: true,
+    enumerable: true,
+    writable: true,
+    value: patchValue,
   })
+
+/**
+ * @todo move to fnkit
+ * will not change the original object
+ * @param originalObject
+ * @param patchObject
+ * @param attachRule
+ * @returns void
+ */
+export function assignObjectWithConfigs<T extends object>(
+  originalObject: T,
+  patchObject: Partial<T>,
+  attachRule?: AttachFn,
+): T
+export function assignObjectWithConfigs<T extends object>(
+  originalObject: T,
+  patchObject: object,
+  attachRule?: AttachFn,
+): T
+export function assignObjectWithConfigs<T extends object>(
+  originalObject: T,
+  patchObject: Partial<T>,
+  attachRule: AttachFn = simpleObectAssign,
+): T {
+  Reflect.ownKeys(patchObject).forEach((key) => {
+    const originalValue = Reflect.get(originalObject, key)
+    const patchValue = Reflect.get(patchObject, key)
+    if (isObjectLike(originalValue) && isObjectLike(patchValue)) {
+      assignObjectWithConfigs(originalValue, patchValue, attachRule)
+    } else {
+      attachRule({ key, obj: originalObject, originalValue, patchValue })
+    }
+  })
+  return originalObject
 }
-
-// function forEachObjectDescriptor<T extends object>(obj: T, handler: (entry: [keyof T, ]) => void) {
-//   Reflect.ownKeys(obj).forEach((key) => {
-//     handler([key as keyof T, Object.getOwnPropertyDescriptor(obj, key)!])
-//   })
-// }
-
-// function recursivelyAssignObject(obj: object, propertyPairs: object, onAssign?: (key: keyof any, value:any, ) => void) {
-//   forEachObjectEntries(propertyPairs, ([key, value]) => {
-//     if (isObjectLike(value)) {
-//       recursivelyAssignObject(obj[key], value)
-//     } else {
-//       assignObject(obj, { [key]: value })
-//     }
-//   })
-// }
-
-// function recursively
