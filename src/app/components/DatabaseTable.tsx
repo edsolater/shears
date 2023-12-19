8935761d6b20a7327f74cdc17d77916143850b78
zhanggongying <edsolater@qq.com>
2023-12-19 19:00:26 +0800

    🚧 database is configable

--- src/app/components/DatabaseTable.tsx
+++ src/app/components/DatabaseTable.tsx
@@ -1,22 +1,38 @@
+import { createMemo } from 'solid-js'
 import {
   Box,
+  Button,
   Col,
   CollapseBox,
   CollapseBoxProps,
+  Group,
+  Icon,
   KitProps,
   List,
+  Loop,
   Piv,
   PivChild,
   PivProps,
+  Row,
+  Text,
   createRef,
+  icssLabelTitle,
+  icssSubContent,
+  icss_clickable,
   icss_cyberpenkBackground,
   icss_cyberpenkBorder,
   useElementSize,
   useKitProps,
 } from '../../packages/pivkit'
-import { BoardTitle } from './BoardTitle'
+import { PoolItemFaceDetailInfoBoard } from '../pageComponents/PoolPage'
 import { ItemList } from '../utils/dataTransmit/itemMethods'
-import { createMemo } from 'solid-js'
+import toUsdVolume from '../utils/format/toUsdVolume'
+import { BoardTitle } from './BoardTitle'
+
+type TabelCellConfigs<T> = {
+  name: string
+  get: (item: T) => PivChild
+}[]
 
 /**
  * main page components
@@ -27,17 +43,19 @@ import { createMemo } from 'solid-js'
 export function DatabaseTable<T>(
   kitProps: KitProps<{
     items: ItemList<T>
+
+    tabelCellConfigs: TabelCellConfigs<T>
+
     sectionTitle: string
     tableName?: string
     subtitleDescription?: string
+
     TopMiddle?: PivChild
     TopRight?: PivChild
     TableBodyTopLeft?: PivChild
     TableBodyTopMiddle?: PivChild
     TableBodyTopRight?: PivChild
     CollapseBoxProps?: CollapseBoxProps
-    renderCollapseItemFace?: (item: T) => PivChild
-    renderCollapseItemContent?: (item: T) => PivChild
     renderItem?: (item: T) => PivChild
   }>,
 ) {
@@ -45,7 +63,8 @@ export function DatabaseTable<T>(
   return (
     <Col icss={{ maxHeight: '100%', overflowY: 'hidden' }} shadowProps={shadowProps}>
       <BoardTitle>{props.sectionTitle}</BoardTitle>
-      <CyberPanel icss={{ overflow: 'hidden' }}>
+      <CyberPanel icss={{ overflow: 'hidden', paddingInline: '24px' }}>
+        <Box icss={{}}></Box>
         <List lazy items={props.items} icss={{ maxHeight: '100%', overflowY: 'scroll', overflowX: 'hidden' }}>
           {(item) => (
             <Box icss={{ paddingBlock: '4px' }}>
@@ -54,16 +73,215 @@ export function DatabaseTable<T>(
                 icss={{
                   borderRadius: '20px',
                   overflow: 'hidden',
-                  marginInline: '24px',
                 }}
-                renderFace={props.renderCollapseItemFace?.(item)}
-                renderContent={props.renderCollapseItemContent?.(item)}
+                // need to render multiple times to get the correct height, why not let it be a web component?
+                renderFace={<DatabaseTableItemCollapseFace item={item} tabelCellConfigs={props.tabelCellConfigs} />}
+                renderContent={
+                  <DatabaseTableItemCollapseContent item={item} tabelCellConfigs={props.tabelCellConfigs} />
+                }
               />
             </Box>
           )}
         </List>
       </CyberPanel>
     </Col>
+  )
+}
+
+function DatabaseTableItemCollapseFace<T>(kitProps: KitProps<{ item: T; tabelCellConfigs: TabelCellConfigs<T> }>) {
+  const { props, shadowProps } = useKitProps(kitProps, { name: 'PoolItemFace' })
+  const isFavourite = () => false
+  return (
+    <Row
+      shadowProps={shadowProps}
+      icss={{
+        paddingBlock: '20px',
+        background: '#141041',
+        gap: '8px',
+        display: 'grid',
+        gridTemplateColumns: 'auto 1.6fr 1fr 1fr 1fr .8fr auto',
+        transition: 'all 150ms',
+      }}
+    >
+      <Box
+        icss={{
+          width: '24px',
+          alignSelf: 'center',
+          marginLeft: '24px',
+          marginRight: '8px',
+        }}
+      >
+        <Icon
+          src={isFavourite() ? '/icons/misc-star-filled.svg' : '/icons/misc-star-empty.svg'}
+          onClick={({ ev }) => {
+            ev.stopPropagation()
+            // onUnFavorite?.(deAccessify(props.item).ammId)
+            // onStartFavorite?.(deAccessify(props.item).ammId)
+          }}
+          icss={[icss_clickable, { margin: 'auto', alignSelf: 'center' }]}
+        />
+      </Box>
+
+      {/* <PoolItemFaceTokenAvatarLabel info={kitProps.item} /> */}
+
+      <Loop of={props.tabelCellConfigs}>
+        {(config) => {
+          const i = props.item as T // TODO: fix this without `as`
+          const value = config.get(i)
+          return (
+            <Box icss={{ display: 'flex', alignItems: 'center' }}>
+              <PoolItemFaceDetailInfoBoard name={config.name} value={value} />
+            </Box>
+          )
+        }}
+      </Loop>
+      {/*<TextInfoItem
+        name={`Volume(${timeBasis})`}
+        value={
+          timeBasis === '24H'
+            ? toUsdVolume((correspondingJsonInfo ?? info).volume24h, { autoSuffix: isTablet, decimalPlace: 0 })
+            : timeBasis === '7D'
+              ? toUsdVolume((correspondingJsonInfo ?? info).volume7d, { autoSuffix: isTablet, decimalPlace: 0 })
+              : toUsdVolume((correspondingJsonInfo ?? info).volume30d, { autoSuffix: isTablet, decimalPlace: 0 })
+        }
+      />
+      <TextInfoItem
+        name={`Fees(${timeBasis})`}
+        value={
+          timeBasis === '24H'
+            ? toUsdVolume((correspondingJsonInfo ?? info).fee24h, { autoSuffix: isTablet, decimalPlace: 0 })
+            : timeBasis === '7D'
+              ? toUsdVolume((correspondingJsonInfo ?? info).fee7d, { autoSuffix: isTablet, decimalPlace: 0 })
+              : toUsdVolume((correspondingJsonInfo ?? info).fee30d, { autoSuffix: isTablet, decimalPlace: 0 })
+        }
+      />
+      <TextInfoItem
+        name={`APR(${timeBasis})`}
+        value={
+          timeBasis === '24H'
+            ? formatApr((correspondingJsonInfo ?? info).apr24h, { alreadyPercented: true })
+            : timeBasis === '7D'
+              ? formatApr((correspondingJsonInfo ?? info).apr7d, { alreadyPercented: true })
+              : formatApr((correspondingJsonInfo ?? info).apr30d, { alreadyPercented: true })
+        }
+      />
+      <Grid className='w-9 h-9 mr-8 place-items-center'>
+        <Icon size='sm' heroIconName={`${open ? 'chevron-up' : 'chevron-down'}`} />
+      </Grid> */}
+    </Row>
+  )
+}
+
+function DatabaseTableItemCollapseContent<T>(kitProps: KitProps<{ item: T; tabelCellConfigs: TabelCellConfigs<T> }>) {
+  const { props, shadowProps } = useKitProps(kitProps, { name: 'PoolItemContent' })
+  const isFavourite = () => false
+  return (
+    <Row
+      icss={{
+        background: 'linear-gradient(126.6deg, rgba(171, 196, 255, 0.12), rgb(171 196 255 / 4%) 100%)',
+        justifyContent: 'space-between',
+      }}
+    >
+      <Group
+        name='collapse detail infos'
+        icss={{
+          paddingBlock: '20px',
+          paddingInline: '24px',
+          gap: '4vw',
+          flexGrow: 1,
+          justifyContent: 'space-between',
+        }}
+      >
+        <Row>
+          <Box icss={{ flexGrow: 1 }}>
+            <Text icss={[{ marginBottom: '4px' }, icssLabelTitle]}>Your Liquidity</Text>
+            <Text icss={icssSubContent}>
+              {toUsdVolume(11000)}
+              {/* {toUsdVolume(toTotalPrice(balances[info.lpMint], prices[info.lpMint]))} */}
+            </Text>
+            {/* <div className="text-[rgba(171,196,255,0.5)] font-medium text-sm mobile:text-2xs">
+              {isHydratedPoolItemInfo(info) ? toString(balances[info.lpMint] ?? 0) + ' LP' : '--'}
+            </div> */}
+          </Box>
+        </Row>
+      </Group>
+
+      <Row
+        icss={{
+          paddingBlock: '8px',
+          paddingInline: '24px',
+          gap: '12px',
+          justifyContent: 'center',
+        }}
+      >
+        <Button
+          onClick={() => {
+            // routeTo('/liquidity/add', {
+            //   queryProps: {
+            //     ammId: info.ammId,
+            //   },
+            // })
+          }}
+        >
+          Add Liquidity
+        </Button>
+        {/* <Tooltip>
+          <Icon
+            size='smi'
+            iconSrc='/icons/pools-farm-entry.svg'
+            className={`grid place-items-center w-10 h-10 mobile:w-8 mobile:h-8 ring-inset ring-1 mobile:ring-1 ring-[rgba(171,196,255,.5)] rounded-xl mobile:rounded-lg text-[rgba(171,196,255,.5)] clickable-filter-effect ${
+              correspondingFarm ? 'clickable' : 'not-clickable'
+            }`}
+            onClick={() => {
+              routeTo('/farms', {
+                //@ts-expect-error no need to care about enum of this error
+                queryProps: objectShakeFalsy({
+                  currentTab: correspondingFarm?.category ? capitalize(correspondingFarm?.category) : undefined,
+                  newExpandedItemId: toPubString(correspondingFarm?.id),
+                  searchText: info.lpMint,
+                }),
+              })
+            }}
+          />
+          <Tooltip.Panel>Farm</Tooltip.Panel>
+        </Tooltip>
+        <Tooltip>
+          <Icon
+            size='smi'
+            iconSrc='/icons/pools-remove-liquidity-entry.svg'
+            className={`grid place-items-center w-10 h-10 mobile:w-8 mobile:h-8 ring-inset ring-1 mobile:ring-1 ring-[rgba(171,196,255,.5)] rounded-xl mobile:rounded-lg text-[rgba(171,196,255,.5)] ${
+              hasLp ? 'opacity-100 clickable clickable-filter-effect' : 'opacity-50 not-clickable'
+            }`}
+            onClick={() => {
+              hasLp &&
+                routeTo('/liquidity/add', {
+                  queryProps: {
+                    ammId: info.ammId,
+                    mode: 'removeLiquidity',
+                  },
+                })
+            }}
+          />
+          <Tooltip.Panel>Remove Liquidity</Tooltip.Panel>
+        </Tooltip> */}
+        {/* <Tooltip>
+          <Icon
+            iconSrc='/icons/msic-swap-h.svg'
+            size='smi'
+            className='grid place-items-center w-10 h-10 mobile:w-8 mobile:h-8 ring-inset ring-1 mobile:ring-1 ring-[rgba(171,196,255,.5)] rounded-xl mobile:rounded-lg text-[rgba(171,196,255,.5)] clickable clickable-filter-effect'
+            onClick={() => {
+              routeTo('/swap', {
+                queryProps: {
+                  coin1: info.base,
+                  coin2: info.quote,
+                },
+              })
+            }}
+          />
+          <Tooltip.Panel>Swap</Tooltip.Panel>
+        </Tooltip> */}
+      </Row>
+    </Row>
   )
 }
 
--- src/app/pageComponents/PoolPage.tsx
+++ src/app/pageComponents/PoolPage.tsx
@@ -1,107 +1,15 @@
-import {
-  Box,
-  Button,
-  Group,
-  Icon,
-  KitProps,
-  Piv,
-  Row,
-  Text,
-  icssLabelTitle,
-  icssSubContent,
-  icss_clickable,
-  icss_label,
-  useKitProps,
-} from '../../packages/pivkit'
-import { icssFont } from '../../packages/pivkit/styles/icssRules/fondation'
+import { Box, KitProps, Text, useKitProps } from '../../packages/pivkit'
 import { DatabaseTable } from '../components/DatabaseTable'
 import { Token } from '../components/TokenProps'
 import { createStorePropertySignal } from '../stores/data/store'
 import { PairInfo } from '../stores/data/types/pairs'
-import toUsdVolume from '../utils/format/toUsdVolume'
 
-type PoolItemFaceProps = KitProps<{ item: PairInfo }>
-
-function PoolItemFace(kitProps: PoolItemFaceProps) {
-  const { props, shadowProps } = useKitProps(kitProps, { name: 'PoolItemFace' })
-  const isFavourite = () => false
-  return (
-    <Row
-      shadowProps={shadowProps}
-      icss={{
-        paddingBlock: '20px',
-        background: '#141041',
-        gap: '8px',
-        display: 'grid',
-        gridTemplateColumns: 'auto 1.6fr 1fr 1fr 1fr .8fr auto',
-        transition: 'all 150ms',
-      }}
-    >
-      <Box
-        icss={{
-          width: '24px',
-          alignSelf: 'center',
-          marginLeft: '24px',
-          marginRight: '8px',
-        }}
-      >
-        <Icon
-          src={isFavourite() ? '/icons/misc-star-filled.svg' : '/icons/misc-star-empty.svg'}
-          onClick={({ ev }) => {
-            ev.stopPropagation()
-            // onUnFavorite?.(deAccessify(props.item).ammId)
-            // onStartFavorite?.(deAccessify(props.item).ammId)
-          }}
-          icss={[icss_clickable, { margin: 'auto', alignSelf: 'center' }]}
-        />
-      </Box>
-
-      <PoolItemFaceTokenAvatarLabel info={kitProps.item} />
-
-      <PoolItemFaceDetailInfoBoard name='Liquidity' value={1231} />
-      {/*<TextInfoItem
-        name={`Volume(${timeBasis})`}
-        value={
-          timeBasis === '24H'
-            ? toUsdVolume((correspondingJsonInfo ?? info).volume24h, { autoSuffix: isTablet, decimalPlace: 0 })
-            : timeBasis === '7D'
-              ? toUsdVolume((correspondingJsonInfo ?? info).volume7d, { autoSuffix: isTablet, decimalPlace: 0 })
-              : toUsdVolume((correspondingJsonInfo ?? info).volume30d, { autoSuffix: isTablet, decimalPlace: 0 })
-        }
-      />
-      <TextInfoItem
-        name={`Fees(${timeBasis})`}
-        value={
-          timeBasis === '24H'
-            ? toUsdVolume((correspondingJsonInfo ?? info).fee24h, { autoSuffix: isTablet, decimalPlace: 0 })
-            : timeBasis === '7D'
-              ? toUsdVolume((correspondingJsonInfo ?? info).fee7d, { autoSuffix: isTablet, decimalPlace: 0 })
-              : toUsdVolume((correspondingJsonInfo ?? info).fee30d, { autoSuffix: isTablet, decimalPlace: 0 })
-        }
-      />
-      <TextInfoItem
-        name={`APR(${timeBasis})`}
-        value={
-          timeBasis === '24H'
-            ? formatApr((correspondingJsonInfo ?? info).apr24h, { alreadyPercented: true })
-            : timeBasis === '7D'
-              ? formatApr((correspondingJsonInfo ?? info).apr7d, { alreadyPercented: true })
-              : formatApr((correspondingJsonInfo ?? info).apr30d, { alreadyPercented: true })
-        }
-      />
-      <Grid className='w-9 h-9 mr-8 place-items-center'>
-        <Icon size='sm' heroIconName={`${open ? 'chevron-up' : 'chevron-down'}`} />
-      </Grid> */}
-    </Row>
-  )
-}
-
-function PoolItemFaceDetailInfoBoard(kitProps: KitProps<{ name: string; value?: any }>) {
+export function PoolItemFaceDetailInfoBoard(kitProps: KitProps<{ name: string; value?: any }>) {
   const { props, shadowProps } = useKitProps(kitProps, { name: 'DatabaseListItemFaceDetailInfoBoard' })
   return <Text shadowProps={shadowProps}>{props.value || '--'}</Text>
 }
 
-function PoolItemFaceTokenAvatarLabel(kitProps: KitProps<{ info?: PairInfo }>) {
+export function PoolItemFaceTokenAvatarLabel(kitProps: KitProps<{ info?: PairInfo }>) {
   const { props, shadowProps } = useKitProps(kitProps, { name: 'DatabaseListItemFaceTokenAvatarLabel' })
   return (
     <Box>
@@ -110,130 +18,17 @@ function PoolItemFaceTokenAvatarLabel(kitProps: KitProps<{ info?: PairInfo }>) {
   )
 }
 
-type PoolItemContentProps<T> = KitProps<{ item: T }>
-
-function PoolItemContent<T>(kitProps: PoolItemContentProps<T>) {
-  const { props, shadowProps } = useKitProps(kitProps, { name: 'PoolItemContent' })
-  const isFavourite = () => false
-  return (
-    <Row
-      icss={{
-        background: 'linear-gradient(126.6deg, rgba(171, 196, 255, 0.12), rgb(171 196 255 / 4%) 100%)',
-        justifyContent: 'space-between',
-      }}
-    >
-      <Group
-        name='collapse detail infos'
-        icss={{
-          paddingBlock: '20px',
-          paddingInline: '24px',
-          gap: '4vw',
-          flexGrow: 1,
-          justifyContent: 'space-between',
-        }}
-      >
-        <Row>
-          <Box icss={{ flexGrow: 1 }}>
-            <Text icss={[{ marginBottom: '4px' }, icssLabelTitle]}>Your Liquidity</Text>
-            <Text icss={icssSubContent}>
-              {toUsdVolume(11000)}
-              {/* {toUsdVolume(toTotalPrice(balances[info.lpMint], prices[info.lpMint]))} */}
-            </Text>
-            {/* <div className="text-[rgba(171,196,255,0.5)] font-medium text-sm mobile:text-2xs">
-              {isHydratedPoolItemInfo(info) ? toString(balances[info.lpMint] ?? 0) + ' LP' : '--'}
-            </div> */}
-          </Box>
-        </Row>
-      </Group>
-
-      <Row
-        icss={{
-          paddingBlock: '8px',
-          paddingInline: '24px',
-          gap: '12px',
-          justifyContent: 'center',
-        }}
-      >
-        <Button
-          onClick={() => {
-            // routeTo('/liquidity/add', {
-            //   queryProps: {
-            //     ammId: info.ammId,
-            //   },
-            // })
-          }}
-        >
-          Add Liquidity
-        </Button>
-        {/* <Tooltip>
-          <Icon
-            size='smi'
-            iconSrc='/icons/pools-farm-entry.svg'
-            className={`grid place-items-center w-10 h-10 mobile:w-8 mobile:h-8 ring-inset ring-1 mobile:ring-1 ring-[rgba(171,196,255,.5)] rounded-xl mobile:rounded-lg text-[rgba(171,196,255,.5)] clickable-filter-effect ${
-              correspondingFarm ? 'clickable' : 'not-clickable'
-            }`}
-            onClick={() => {
-              routeTo('/farms', {
-                //@ts-expect-error no need to care about enum of this error
-                queryProps: objectShakeFalsy({
-                  currentTab: correspondingFarm?.category ? capitalize(correspondingFarm?.category) : undefined,
-                  newExpandedItemId: toPubString(correspondingFarm?.id),
-                  searchText: info.lpMint,
-                }),
-              })
-            }}
-          />
-          <Tooltip.Panel>Farm</Tooltip.Panel>
-        </Tooltip>
-        <Tooltip>
-          <Icon
-            size='smi'
-            iconSrc='/icons/pools-remove-liquidity-entry.svg'
-            className={`grid place-items-center w-10 h-10 mobile:w-8 mobile:h-8 ring-inset ring-1 mobile:ring-1 ring-[rgba(171,196,255,.5)] rounded-xl mobile:rounded-lg text-[rgba(171,196,255,.5)] ${
-              hasLp ? 'opacity-100 clickable clickable-filter-effect' : 'opacity-50 not-clickable'
-            }`}
-            onClick={() => {
-              hasLp &&
-                routeTo('/liquidity/add', {
-                  queryProps: {
-                    ammId: info.ammId,
-                    mode: 'removeLiquidity',
-                  },
-                })
-            }}
-          />
-          <Tooltip.Panel>Remove Liquidity</Tooltip.Panel>
-        </Tooltip> */}
-        {/* <Tooltip>
-          <Icon
-            iconSrc='/icons/msic-swap-h.svg'
-            size='smi'
-            className='grid place-items-center w-10 h-10 mobile:w-8 mobile:h-8 ring-inset ring-1 mobile:ring-1 ring-[rgba(171,196,255,.5)] rounded-xl mobile:rounded-lg text-[rgba(171,196,255,.5)] clickable clickable-filter-effect'
-            onClick={() => {
-              routeTo('/swap', {
-                queryProps: {
-                  coin1: info.base,
-                  coin2: info.quote,
-                },
-              })
-            }}
-          />
-          <Tooltip.Panel>Swap</Tooltip.Panel>
-        </Tooltip> */}
-      </Row>
-    </Row>
-  )
-}
-
 export default function PoolsPage() {
   const pairInfos = createStorePropertySignal((s) => s.pairInfos)
+  const mockItems = [
+    { name: 'se', address: '1', detail: 'detail' },
+    { name: 'lisdf', address: '33', detail: 'detail3333' },
+  ] satisfies {
+    name: string
+    address: string
+    detail?: string
+  }[]
   return (
-    <DatabaseTable
-      sectionTitle='Pools'
-      items={pairInfos}
-      // need to render multiple times to get the correct height, why not let it be a web component?
-      renderCollapseItemFace={(item) => <PoolItemFace item={item} />}
-      renderCollapseItemContent={(item) => <PoolItemContent item={item} />}
-    />
+    <DatabaseTable sectionTitle='Pools' items={mockItems} tabelCellConfigs={[{ name: 'Pool', get: (i) => i.name }]} />
   )
 }
