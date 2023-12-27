efb05ea6379bd90feadcfa2bc38cb679bf696381
zhanggongying <edsolater@qq.com>
2023-12-27 15:33:12 +0800

    🧪 create test method

---
+++ src/packages/pivkit/utils/measureTimePerformance.ts
@@ -0,0 +1,14 @@
+import { AnyFn } from '@edsolater/fnkit'
+
+export function measureTimePerformance() {
+  const allMeasuredTime = new Map<string, { t: number }>()
+  function run(name: string, code: AnyFn) {
+    const startTime = performance.now()
+    const result = code()
+    const endTime = performance.now()
+    allMeasuredTime.set(name, { t: (allMeasuredTime.get(name)?.t ?? 0) + (endTime - startTime) })
+    console.log(name, allMeasuredTime.get(name)?.t)
+    return result
+  }
+  return { run }
+}
