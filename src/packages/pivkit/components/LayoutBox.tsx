700630c896967fe8c4e772ce0c3878b6e56192d1
edsolater <edsolater@qq.com>
2024-01-05 00:41:24 +0800

    💡 an idea of `<LayoutBox>`

---
+++ src/packages/pivkit/components/LayoutBox.tsx
@@ -0,0 +1,25 @@
+import { JSXElement } from 'solid-js'
+import { KitProps, useKitProps } from '../createKit'
+import { Piv } from '../piv'
+export type Temp = any
+type LayoutPreset = Temp
+
+export interface LayoutBoxProps {
+  in?: LayoutPreset
+}
+
+export type LayoutBoxKitProps = KitProps<LayoutBoxProps>
+
+/**
+ * Render a Box for layout
+ */
+export function LayoutBox(rawProps: LayoutBoxKitProps) {
+  const { props } = useKitProps(rawProps, { name: 'LayoutBox' })
+  /* ---------------------------------- props --------------------------------- */
+  return (
+    <Piv shadowProps={props} icss={{ display: 'flex', alignItems: 'center' }}>
+      {/* TODO: porps.children should be normal  */}
+      <>{props.children}</>
+    </Piv>
+  )
+}
