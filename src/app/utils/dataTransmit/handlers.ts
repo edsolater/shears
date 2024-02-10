8878d65ee7f4c0b196ff5d1f557c4facdc0ba58b
zhanggongying <edsolater@qq.com>
2024-02-10 14:57:53 +0800

    🚧 decode mutMap

--- src/app/utils/dataTransmit/handlers.ts
+++ src/app/utils/dataTransmit/handlers.ts
@@ -1,7 +1,13 @@
-import { isArray, isObjectLiteral, map } from '@edsolater/fnkit'
+import { isArray, isObject, isObjectLiteral, map } from '@edsolater/fnkit'
 import { rules } from './rules'
 
-export function encode(data: unknown): any {
+export function encode(
+  data: unknown,
+  options?: {
+    /** encode should false */
+    mutate?: boolean
+  },
+): any {
   // try to match rule
   for (const rule of rules) {
     if (rule.canEncode?.(data)) {
@@ -10,13 +16,19 @@ export function encode(data: unknown): any {
   }
 
   // literal need to deeply parse
-  if (isObjectLiteral(data) || isArray(data)) return map(data, (v) => encode(v))
+  if (isObjectLiteral(data) || isArray(data)) return (options?.mutate ? mutMap : map)(data, (v) => encode(v))
 
   // no match rule
   return data
 }
 
-export function decode(data: unknown): any {
+export function decode(
+  data: unknown,
+  options?: {
+    /** decode should true */
+    mutate?: boolean
+  },
+): any {
   // try to match rule
   for (const rule of rules) {
     if (rule.canDecode?.(data)) {
@@ -25,8 +37,22 @@ export function decode(data: unknown): any {
   }
 
   // literal need to deeply parse
-  if (isObjectLiteral(data) || isArray(data)) return map(data, (v) => decode(v))
+  if (isObjectLiteral(data) || isArray(data)) return (options?.mutate ? mutMap : map)(data, (v) => decode(v)) // FIXME: immuteable will cause performance issue
 
   // no match rule
   return data
+}
+
+/** TODO: move to fnkit */
+function mutMap(collection: object | any[], mapCallback: (v: any) => any) {
+  if (isArray(collection)) {
+    for (let i = 0; i < collection.length; i++) {
+      collection[i] = mapCallback(collection[i])
+    }
+  } else if (isObject(collection)) {
+    for (const key in collection) {
+      collection[key] = mapCallback(collection[key])
+    }
+  }
+  return collection
 }
