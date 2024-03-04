3d1a304bf57e519c43897cedbfaf012e58450533
edsolater <edsolater@qq.com>
2024-03-04 21:07:34 +0800

    💩 pending reward

--- src/app/pages/clmm.tsx
+++ src/app/pages/clmm.tsx
@@ -1,6 +1,6 @@
-import { add, count, div, mul, toStringNumber, type Numberish } from '@edsolater/fnkit'
+import { count, div, toStringNumber, type Numberish } from '@edsolater/fnkit'
 import { Box, Col, KitProps, Text, useKitProps } from '@edsolater/pivkit'
-import { createEffect, createMemo, onMount, type Accessor } from 'solid-js'
+import { createEffect, onMount, type Accessor } from 'solid-js'
 import { createStore, reconcile } from 'solid-js/store'
 import { useShuckValue } from '../../packages/conveyor/solidjsAdapter/useShuck'
 import { Button, cssOpacity, Loop, parseICSSToClassName, Row, Tab, TabList, Tabs } from '../../packages/pivkit'
@@ -17,7 +17,6 @@ import { Token } from '../components/TokenProps'
 import { TokenSymbolPair } from '../components/TokenSymbolPair'
 import { loadClmmInfos } from '../stores/data/portActions/loadClmmInfos_main'
 import { useToken } from '../stores/data/shapeParser/token'
-import { useTokenPrice } from '../stores/data/shapeParser/tokenPrice'
 import {
   allClmmTabs,
   createStorePropertySignal,
@@ -28,8 +27,8 @@ import {
 import type { ClmmInfo, ClmmUserPositionAccount } from '../stores/data/types/clmm'
 import type { PairInfo } from '../stores/data/types/pairs'
 import { toRenderable } from '../utils/common/toRenderable'
-import type { Price, USDVolume } from '../utils/dataStructures/type'
 import toUsdVolume from '../utils/format/toUsdVolume'
+import { useClmmUserPositionAccount } from '../stores/data/featureHooks/useClmmUserPositionAccount'
 
 export const icssClmmItemRow = parseICSSToClassName({ paddingBlock: '4px' })
 export const icssClmmItemRowCollapse = parseICSSToClassName({
@@ -152,12 +151,12 @@ export default function ClmmsPage() {
  * comopnent render clmm user position account
  */
 function ClmmUserPositionAccountRow(props: { clmmInfo: ClmmInfo; account: ClmmUserPositionAccount }) {
-  const { rangeText, userLiquidity } = useClmmUserPositionAccount(props.clmmInfo, props.account)
+  const { rangeName, userLiquidity } = useClmmUserPositionAccount(props.clmmInfo, props.account)
   return (
     <Row icss={{ gap: '20px', margin: '8px 32px' }}>
       {/* range */}
       <Box icss={{ borderRadius: '12px' }}>
-        <Text>{rangeText()}</Text>
+        <Text>{rangeName()}</Text>
       </Box>
 
       {/* my liquidity */}
@@ -187,51 +186,6 @@ function ClmmPageActionHandlersBlock(props: { className?: string }) {
   return <Text>actions</Text>
 }
 
-/**
- * hooks
- * hydrate {@link ClmmUserPositionAccount} to ui used data
- */
-function useClmmUserPositionAccount(clmmInfo: ClmmInfo, userPositionAccount: ClmmUserPositionAccount) {
-  const tokenA = useToken(() => clmmInfo.base)
-  const tokenB = useToken(() => clmmInfo.quote)
-  const priceA = useTokenPrice(() => clmmInfo.base)
-  const priceB = useTokenPrice(() => clmmInfo.quote)
-
-  const userLiquidity = createMemo(() => {
-    const tokenAPrices = priceA()
-    const tokenBPrices = priceB()
-    if (!tokenAPrices || !tokenBPrices) return undefined
-    const tokenADecimal = tokenA.decimals
-    const tokenBDecimal = tokenB.decimals
-    const amountABN = userPositionAccount.amountBaseBN
-    const amountBBN = userPositionAccount.amountQuoteBN
-    if (amountABN === undefined || amountBBN === undefined) return undefined
-    return getLiquidityVolume(tokenAPrices, tokenBPrices, tokenADecimal, tokenBDecimal, amountABN, amountBBN)
-  })
-
-  const rangeText = createMemo(
-    () =>
-      `${toRenderable(userPositionAccount.priceLower, { decimals: 4 })}-${toRenderable(userPositionAccount.priceUpper, { decimals: 4 })}`,
-  )
-  return { rangeText, userLiquidity }
-}
-
-/**
- * used in {@link useClmmUserPositionAccount}
- */
-function getLiquidityVolume(
-  tokenAPrices: Price,
-  tokenBPrices: Price,
-  tokenADecimal: number,
-  tokenBDecimal: number,
-  amountABN: Numberish,
-  amountBBN: Numberish,
-): USDVolume {
-  const amountA = applyDecimal(amountABN, tokenADecimal)
-  const amountB = applyDecimal(amountBBN, tokenBDecimal)
-  const wholeLiquidity = add(mul(amountA, tokenAPrices), mul(amountB, tokenBPrices))
-  return wholeLiquidity
-}
 // 🔥 already in pivkit
 /** T must is object */
 function createStoreFromAccessor<T extends object>(
@@ -247,6 +201,6 @@ function createStoreFromAccessor<T extends object>(
   return store
 }
 
-function applyDecimal(n: Numberish, decimal: number): Numberish {
+export function applyDecimal(n: Numberish, decimal: number): Numberish {
   return div(n, Math.pow(10, decimal)) // TODO: should be faster
 }
---
+++ src/app/stores/data/featureHooks/useClmmUserPositionAccount.ts
@@ -0,0 +1,160 @@
+import { add, get, isPositive, mul, shakeNil, type Numberish } from '@edsolater/fnkit'
+import { createMemo, createSignal, on } from 'solid-js'
+import { useToken } from '../shapeParser/token'
+import { useTokenPrice } from '../shapeParser/tokenPrice'
+import type { ClmmInfo, ClmmUserPositionAccount } from '../types/clmm'
+import { toRenderable } from '../../../utils/common/toRenderable'
+import type { Price, USDVolume } from '../../../utils/dataStructures/type'
+import { applyDecimal } from '../../../pages/clmm'
+import { shuck_tokenPrices, shuck_tokens } from '../store'
+import { useShuckValue } from '../../../../packages/conveyor/solidjsAdapter/useShuck'
+import { toTokenAmount, type TokenAmount } from '../../../utils/dataStructures/TokenAmount'
+
+/**
+ * hooks
+ * hydrate {@link ClmmUserPositionAccount} to ui used data
+ */
+export function useClmmUserPositionAccount(clmmInfo: ClmmInfo, userPositionAccount: ClmmUserPositionAccount) {
+  const tokenA = useToken(() => clmmInfo.base)
+  const tokenB = useToken(() => clmmInfo.quote)
+  const priceA = useTokenPrice(() => clmmInfo.base)
+  const priceB = useTokenPrice(() => clmmInfo.quote)
+  const pricesMap = useShuckValue(shuck_tokenPrices)
+  const tokens = useShuckValue(shuck_tokens)
+
+  const userLiquidity = createMemo(() => {
+    const tokenAPrices = priceA()
+    const tokenBPrices = priceB()
+    if (!tokenAPrices || !tokenBPrices) return undefined
+    const tokenADecimal = tokenA.decimals
+    const tokenBDecimal = tokenB.decimals
+    const amountABN = userPositionAccount.amountBaseBN
+    const amountBBN = userPositionAccount.amountQuoteBN
+    if (amountABN === undefined || amountBBN === undefined) return undefined
+    return getLiquidityVolume(tokenAPrices, tokenBPrices, tokenADecimal, tokenBDecimal, amountABN, amountBBN)
+  })
+
+  const [hasRewardTokenAmount, setHasRewardTokenAmount] = createSignal(false)
+
+  const rangeName = createMemo(
+    () =>
+      `${toRenderable(userPositionAccount.priceLower, { decimals: 4 })}-${toRenderable(userPositionAccount.priceUpper, { decimals: 4 })}`,
+  )
+
+  const rewardsAmountsWithFees: () => {
+    tokenAmount: TokenAmount | undefined
+    price: Price | undefined
+  }[] = createMemo(
+    () =>
+      userPositionAccount?.rewardInfos.map((info) => {
+        if (isPositive(info.penddingReward)) {
+          setHasRewardTokenAmount(true)
+        }
+        const price = info.token && pricesMap()?.get(info.token)
+
+        const t = info.token ? get(tokens(), info.token) : undefined
+        return {
+          tokenAmount: t && info.penddingReward ? toTokenAmount(t, info.penddingReward) : undefined,
+          price: price,
+        }
+      }) ?? [],
+  )
+
+  const feesAmountsWithFees: () => {
+    tokenAmount: TokenAmount | undefined
+    price: Price | undefined
+  }[] = createMemo(() => {
+    const t1 = userPositionAccount.tokenBase ? get(tokens(), userPositionAccount.tokenBase) : undefined
+    const t2 = userPositionAccount.tokenQuote ? get(tokens(), userPositionAccount.tokenQuote) : undefined
+    return userPositionAccount
+      ? [
+          {
+            tokenAmount:
+              t1 && userPositionAccount.tokenFeeAmountBase
+                ? toTokenAmount(t1, userPositionAccount.tokenFeeAmountBase)
+                : undefined,
+            price: userPositionAccount.tokenBase && pricesMap()?.get(userPositionAccount.tokenBase),
+          },
+          {
+            tokenAmount:
+              t2 && userPositionAccount.tokenFeeAmountQuote
+                ? toTokenAmount(t2, userPositionAccount.tokenFeeAmountQuote)
+                : undefined,
+            price: userPositionAccount.tokenQuote && pricesMap()?.get(userPositionAccount.tokenQuote),
+          },
+        ]
+      : []
+  })
+
+  const hasFeeTokenAmount = createMemo(
+    () => isPositive(userPositionAccount.tokenFeeAmountBase) || isPositive(userPositionAccount.tokenFeeAmountQuote),
+  )
+
+  const pendingTotalWithFees = createMemo(() =>
+    rewardsAmountsWithFees()
+      .concat(feesAmountsWithFees())
+      .reduce(
+        (acc, { tokenAmount, price }) => {
+          if (!tokenAmount || !price) return acc
+          return add(acc ?? 0, mul(tokenAmount.amount, price))
+        },
+        undefined as Numberish | undefined,
+      ),
+  )
+
+  // const pendingRewardAmount = createMemo(
+  //   on([rewardsAmountsWithFees, feesAmountsWithFees], async () => {
+  //     const mints = shakeNil(
+  //       rewardsAmountsWithFees()
+  //         .concat(feesAmountsWithFees())
+  //         .map((i) => i.tokenAmount?.tokenMint),
+  //     )
+
+  //     const [epochInfo, mintInfos] = mints.some((m) => !isCurrentToken2022(m))
+  //       ? []
+  //       : await Promise.all([getEpochInfo(), getMultiMintInfos({ mints })])
+
+  //     const ams = await asyncMap(
+  //       rewardsAmountsWithFees().concat(feesAmountsWithFees()),
+  //       async ({ tokenAmount, ...rest }) => {
+  //         if (!tokenAmount) return
+  //         const feeInfo = await getTransferFeeInfo({
+  //           tokenAmount,
+  //           fetchedEpochInfo: epochInfo,
+  //           fetchedMints: mintInfos,
+  //         })
+  //         return { ...rest, tokenAmount: feeInfo?.pure }
+  //       },
+  //     )
+  //     return shakeNil(ams).reduce(
+  //       (acc, { tokenAmount, price }) => {
+  //         if (!tokenAmount || !price) return acc
+  //         return add(acc ?? toFraction(0), mul(tokenAmount.amount, price))
+  //       },
+  //       undefined as Numberish | undefined,
+  //     )
+  //   }),
+  // )
+
+  const isHarvestable = createMemo(() =>
+    isPositive(pendingTotalWithFees()) || hasRewardTokenAmount() || hasFeeTokenAmount() ? true : false,
+  )
+  return { rangeName, userLiquidity, hasRewardTokenAmount, isHarvestable }
+}
+
+/**
+ * used in {@link useClmmUserPositionAccount}
+ */
+function getLiquidityVolume(
+  tokenAPrices: Price,
+  tokenBPrices: Price,
+  tokenADecimal: number,
+  tokenBDecimal: number,
+  amountABN: Numberish,
+  amountBBN: Numberish,
+): USDVolume {
+  const amountA = applyDecimal(amountABN, tokenADecimal)
+  const amountB = applyDecimal(amountBBN, tokenBDecimal)
+  const wholeLiquidity = add(mul(amountA, tokenAPrices), mul(amountB, tokenBPrices))
+  return wholeLiquidity
+}
