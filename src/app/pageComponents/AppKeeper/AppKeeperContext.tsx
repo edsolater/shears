3da910d60ec75877f8f7c1b13c8697a51ce97f5b
edsolater <edsolater@qq.com>
2024-05-28 14:53:57 +0800

    🩹 don't use dynamic context value

--- src/app/pageComponents/AppKeeper/AppKeeperContext.tsx
+++ src/app/pageComponents/AppKeeper/AppKeeperContext.tsx
@@ -2,14 +2,13 @@ import { createComponentContext } from "@edsolater/pivkit"
 import { type Accessor } from "solid-js"
 import type { AppKeeperProps } from "."
 
-export const AppKeeperContext = createComponentContext<
-  {
-    isSideMenuOpen?: Accessor<boolean>
-    isSideMenuFloating?: Accessor<boolean>
-    toggleSideMenu?: () => void
+export const AppKeeperContext = createComponentContext<{
+  isSideMenuOpen?: Accessor<boolean>
+  isSideMenuFloating?: Accessor<boolean>
+  toggleSideMenu?: () => void
 
-    isTopBarOpen?: Accessor<boolean>
-    isTopBarFloating?: Accessor<boolean>
-    toggleTopBar?: () => void
-  } & AppKeeperProps
->()
+  isTopBarOpen?: Accessor<boolean>
+  isTopBarFloating?: Accessor<boolean>
+  toggleTopBar?: () => void
+  props?: AppKeeperProps
+}>()
