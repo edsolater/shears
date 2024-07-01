7d780f2780eccd7e5199ccdc43d09f9f6063764a
edsolater <edsolater@qq.com>
2024-07-01 13:22:34 +0800

    🩹

--- src/app/pages/clmm.tsx
+++ src/app/pages/clmm.tsx
@@ -281,9 +281,9 @@ export default function ClmmsPage() {
             const speed = speedLevel()
             console.log("speed: ", speed)
             if (speed === "flash") {
-              return 1000 * 20
+              return 1000 * 30
             } else if (speed === "quick") {
-              return 1000 * 60
+              return 1000 * 60 * 1.5
             } else {
               return 1000 * 60 * 5
             }
